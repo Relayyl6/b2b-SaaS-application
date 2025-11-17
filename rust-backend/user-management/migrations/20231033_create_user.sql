de8c848b7cd9101c041573e9106b461a18989acd
Leonard <Oseghaleleonard39@gmail.com>
2025-11-17 17:29:13 +0100

    sign in, sign outm sign up are working

--- rust-backend/user-management/migrations/20231033_create_user.sql
+++ rust-backend/user-management/migrations/20231033_create_user.sql
@@ -1,11 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
+CREATE TYPE user_role AS ENUM ('user', 'admin', 'supplier');
+
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email TEXT NOT NULL,
     password TEXT NOT NULL,
     full_name TEXT NOT NULL,
-    role TEXT NOT NULL DEFAULT "user",
+    role user_role NOT NULL DEFAULT 'user',
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW(),
@@ -19,5 +21,3 @@ CREATE TABLE IF NOT EXISTS revoked_tokens (
     token TEXT NOT NULL,
     revoked_at TIMESTAMP DEFAULT NOW()
 );
-
-CREATE TYPE user_role AS ENUM ('user', 'admin', 'supplier');
